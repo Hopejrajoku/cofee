307a3ad071e9870d3f47a274c1156cad0c5f3386
hopejrajoku <hajoku0@gmail.com>
2025-04-18 18:28:39 +0100

    new

---
+++ src/components/header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+
+const Header = () => {
+  return (
+    <section className="bg-[#3b241f] text-white min-h-screen px-4 py-8">
+        <header className="flex justify-between items-center max-w-7xl mx-auto">
+            <div className="flex items-center gap-2">
+                <img src={logo} alt="Logo" className="w-16 h-16 object-conatin" />
+                <div className="text-center">
+                  <h1 className='text-sm font-bold'>COFFEE</h1>
+                  <p className='text-xs'>BORCELLET</p>
+                </div>
+            </div>
+        </header>
+            
+    </section>
+  )
+}
+
+export default Header
