6e4c04c996e3a70bba3510ff22b4c00b5746c905
hopejrajoku <hajoju0@gmail.com>
2025-04-25 09:06:21 -0700

    onnn

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import logo from '../assets/logo.png'
+//import logo from '../assets/logo.png'
 
 const Header = () => {
   return (
-    <section className="bg-[#3b241f] text-white min-h-screen px-4 py-8">
+    <section className="bg-[#3b241f] text-white min-h-screen px-4 py-8 h-12">
         <header className="flex justify-between items-center max-w-7xl mx-auto">
             <div className="flex items-center gap-2">
-                <img src={logo} alt="Logo" className="w-16 h-16 object-conatin" />
+                <img src='' alt="Logo" className="w-16 h-16 object-conatin" />
                 <div className="text-center">
                   <h1 className='text-sm font-bold'>COFFEE</h1>
                   <p className='text-xs'>BORCELLET</p>
